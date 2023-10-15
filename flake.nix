b2e6854cd68b696b22d5464cee346ee3c567e734
Robert Atkey <robert.atkey@strath.ac.uk>
2023-10-15 23:54:56 +0100

    [ build ] pikchr

--- flake.nix
+++ flake.nix
@@ -59,6 +59,7 @@
         devShells.default = pkgs.mkShell {
           inputsFrom = builtins.attrValues packages;
           buildInputs = devPackages ++ [
+            pkgs.pikchr
 #            pkgs.z3
             # You can add packages from nixpkgs here
           ];
