0c38acd366beb1c5121f6096847b769b24ed39e8
Robert Atkey <robert.atkey@strath.ac.uk>
2023-10-15 16:24:23 +0100

    [ code ] Rearrange Sexp_parser interface

--- generalities/sexp_parser.mli
+++ generalities/sexp_parser.mli
@@ -1,3 +1,5 @@
+(** Sexp parsing *)
+
 type sexp = Sexplib.Type.t = Atom of string | List of sexp list
 
 type error = (string, sexp) Annotated.t
@@ -6,7 +8,7 @@ type 'a parser = sexp -> ('a, error) result
 
 type 'a seq_parser
 
-(** Sexp parsers *)
+(** {2 Direct Sexp parsers} *)
 
 val atom : string parser
 
@@ -18,7 +20,7 @@ val tagged : string -> 'a seq_parser -> 'a parser
 
 val ( let+? ) : 'a parser -> ('a -> ('b, string) result) -> 'b parser
 
-(** Sequence parsers *)
+(** {2 Sequence parsers} *)
 
 val ( let* ) : 'a seq_parser -> ('a -> 'b seq_parser) -> 'b seq_parser
 
@@ -26,9 +28,15 @@ val return : 'a -> 'a seq_parser
 
 val fail : string -> 'a seq_parser
 
+val lift : ('a, string) result -> 'a seq_parser
+
+(** {3 Uniform parsers} *)
+
 val one : 'a parser -> 'a seq_parser
 
 val many : 'a parser -> 'a list seq_parser
+
+(** {2 Random access fields} *)
 
 val consume_all : string -> 'a seq_parser -> 'a list seq_parser
 
@@ -36,8 +44,8 @@ val consume_one : string -> 'a seq_parser -> 'a seq_parser
 
 val consume_opt : string -> 'a seq_parser -> 'a option seq_parser
 
+(** {2 Positional} *)
+
 val consume_next : 'a parser -> 'a seq_parser
 
 val assert_nothing_left : unit seq_parser
-
-val lift : ('a, string) result -> 'a seq_parser
