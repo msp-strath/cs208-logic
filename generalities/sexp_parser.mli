a39c196625f975fe4b2b159180a4dcbc03a53704
Robert Atkey <robert.atkey@strath.ac.uk>
2023-10-16 10:06:17 +0100

    [ code ] docs

--- generalities/sexp_parser.mli
+++ generalities/sexp_parser.mli
@@ -1,12 +1,19 @@
 (** Sexp parsing *)
 
 type sexp = Sexplib.Type.t = Atom of string | List of sexp list
+(** The sexpression type *)
 
 type error = (string, sexp) Annotated.t
+(** Parse errors are descriptions annotated with the part of the input
+    affected. *)
 
 type 'a parser = sexp -> ('a, error) result
+(** Direct parsers either extract a value from an {sexp}ression, or
+    fail with an error. *)
 
 type 'a seq_parser
+(** Sequence parsers parse sequences of {sexp}ressions. Sequences can
+    be accessed in random order or sequentially. *)
 
 (** {2 Direct Sexp parsers} *)
 
@@ -33,8 +40,13 @@ val lift : ('a, string) result -> 'a seq_parser
 (** {3 Uniform parsers} *)
 
 val one : 'a parser -> 'a seq_parser
+(** [one p] expects the sequence to consist of exactly one item which can
+    be parsed by [p]. *)
 
 val many : 'a parser -> 'a list seq_parser
+(** [many p] expects the sequence to consist of zero or more items,
+    each of which can be parsed with [p]. On success, it returns the
+    list of parsed results in input order. *)
 
 (** {2 Random access fields} *)
 
@@ -44,8 +56,10 @@ val consume_one : string -> 'a seq_parser -> 'a seq_parser
 
 val consume_opt : string -> 'a seq_parser -> 'a option seq_parser
 
-(** {2 Positional} *)
+(** {2 Positional access} *)
 
 val consume_next : 'a parser -> 'a seq_parser
+
+(** {2 End of sequence} *)
 
 val assert_nothing_left : unit seq_parser
