033cf3cdc27f4cebc2902ecf73885a134df8cb78
Robert Atkey <robert.atkey@strath.ac.uk>
2023-12-14 13:06:53 +0000

    [ code ] some tests for term operations

--- fol_formula/term.ml
+++ fol_formula/term.ml
@@ -1,11 +1,27 @@
 open Sexplib0.Sexp_conv
 
-type t = Var of string | Fun of string * t list [@@deriving sexp]
+type t =
+  | Var of string
+  | Fun of string * t list
+[@@deriving sexp]
+
+(******************************************************************************)
 
 let rec to_string = function
   | Var x -> x
   | Fun ("0", []) -> "0"
   | Fun (f, tms) -> f ^ "(" ^ String.concat ", " (List.map to_string tms) ^ ")"
+
+let%test "to_string_var" =
+  String.equal (to_string (Var "x")) "x"
+
+let%test "to_string_0" =
+  String.equal (to_string (Fun ("0", []))) "0"
+
+let%test "to_string_fun" =
+  String.equal
+    (to_string (Fun ("f", [Fun ("g", []); Var "x"])))
+    "f(g(), x)"
 
 let rec to_latex = function
   | Var x -> Printf.sprintf "\\mathit{%s}" x
@@ -38,12 +54,30 @@ let rec equal t1 t2 = match t1, t2 with
   | _, _ ->
      false
 
+(******************************************************************************)
+
 let rec vars_eq x1 x2 = function
   | [] -> x1 = x2
   | (y1, y2) :: pairs ->
      (x1 = y1 && x2 = y2) || (x1 <> y1 && x2 <> y2 && vars_eq x1 x2 pairs)
 
-(* FIXME: move this to Term *)
+let%test "vars_eq1" =
+  vars_eq "x" "y" ["z","z"; "x","y"]
+
+let%test "vars_eq2" =
+  not @@ vars_eq "x" "y" ["y","x"]
+
+let%test "vars_eq3" =
+  not @@ vars_eq "x" "y" []
+
+let%test "vars_eq4" =
+  not @@ vars_eq "x" "y" ["x","z";"x","y"]
+
+let%test "vars_eq5" =
+  vars_eq "x" "y" ["y","z"; "x","y"]
+
+(******************************************************************************)
+
 let rec equal_open pairs t1 t2 = match t1, t2 with
   | Var x1, Var x2 -> vars_eq x1 x2 pairs
   | Fun (f1, tms1), Fun (f2, tms2) ->
@@ -52,8 +86,13 @@ let rec equal_open pairs t1 t2 = match t1, t2 with
      && List.for_all2 (equal_open pairs) tms1 tms2
   | _, _ -> false
 
-
+(******************************************************************************)
 
 let rec subst x tm = function
   | Var y -> if x = y then tm else Var y
   | Fun (f, tms) -> Fun (f, List.map (subst x tm) tms)
+
+let%test "subst1" =
+  let m = Fun ("f", [Var "x"; Var "x"]) in
+  let n = Fun ("g", [Var "x"]) in
+  equal (subst "x" n m) (Fun ("f", [n; n]))
